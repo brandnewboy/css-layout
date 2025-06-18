3a6c2dad35d8abda37a762a76548c213460309d8
zhangli <2475934003@qq.com>
2025-06-18 19:50:39 +0800

    feat:仿b站

--- src/mobile-adaptation/demo-bilibili/with-px/index.js
+++ src/mobile-adaptation/demo-bilibili/with-px/index.js
@@ -1,8 +1,139 @@
 import './index.scss'
 import { html } from '@/helper'
-document.body.innerHTML = html`
+import Video from '../images/demo.webp'
+
+const template = new window.DOMParser().parseFromString(html`
     <div class="main">
-        content
+        <header class="header">
+            <div class="header-logo">
+                <i class="iconfont icon-BILIBILI"></i>
+            </div>
+            <div class="header-search">
+                <i class="iconfont icon-sousuo"></i>
+            </div>
+            <div class="header-login">
+                <a href="#">登录</a>
+            </div>
+            <div class="header-app">
+                <a href="#">下载 App</a>
+            </div>
+        </header>
+        
+        <nav class="nav">
+            <ul class="nav-list">
+                <li >首页</li>
+                <li class="active">动画</li>
+                <li>番剧</li>
+                <li>国创</li>
+                <li>音乐</li>
+                <li>舞蹈</li>
+                <li>电影</li>
+                <li>番剧</li>
+                <li>国创</li>
+                <li>音乐</li>
+            </ul>
+            <div class="nav-arrow">
+                <i class="iconfont icon-xiangxia"></i>
+            </div>
+        </nav>
+        
+        <main class="main-content">
+            <ul class="videos">
+                <li class="video-item">
+                    <div class="video">
+                        <img class="video-item-img" alt="视频" src="${Video}" />
+                        <span>
+                            <span>
+                                <i class="iconfont icon-shipinbofangliang"></i>
+                                73.7万
+                            </span>
+                            <span>
+                                <i class="iconfont icon-pinglunliang"></i>
+                                5591
+                            </span>
+                        </span>
+                    </div>
+                    <h3>
+                        <a href="#">测试标题测试标题测试标题</a>
+                    </h3>
+                </li>
+                <li class="video-item">
+                    <div class="video">
+                        <img class="video-item-img" alt="视频" src="${Video}" />
+                        <span class="">
+                        <span>
+                                <i class="iconfont icon-shipinbofangliang"></i>
+                                73.7万
+                            </span>
+                            <span>
+                                <i class="iconfont icon-pinglunliang"></i>
+                                5591
+                            </span>
+                    </span>
+                    </div>
+                    <h3>
+                        <a href="#">测试标题测试标题测试标题</a>
+                    </h3>
+                </li>
+                <li class="video-item">
+                    <div class="video">
+                        <img class="video-item-img" alt="视频" src="${Video}" />
+                        <span class="">
+                        <span>
+                                <i class="iconfont icon-shipinbofangliang"></i>
+                                73.7万
+                            </span>
+                            <span>
+                                <i class="iconfont icon-pinglunliang"></i>
+                                5591
+                            </span>
+                    </span>
+                    </div>
+                    <h3>
+                        <a href="#">测试标题测试标题测试标题</a>
+                    </h3>
+                </li>
+                <li class="video-item">
+                    <div class="video">
+                        <img class="video-item-img" alt="视频" src="${Video}" />
+                        <span class="">
+                        <span>
+                                <i class="iconfont icon-shipinbofangliang"></i>
+                                73.7万
+                            </span>
+                            <span>
+                                <i class="iconfont icon-pinglunliang"></i>
+                                5591
+                            </span>
+                    </span>
+                    </div>
+                    <h3>
+                        <a href="#">测试标题测试标题测试标题</a>
+                    </h3>
+                </li>
+                <li class="video-item">
+                    <div class="video">
+                        <img class="video-item-img" alt="视频" src="${Video}" />
+                        <span class="">
+                        <span>
+                                <i class="iconfont icon-shipinbofangliang"></i>
+                                73.7万
+                            </span>
+                            <span>
+                                <i class="iconfont icon-pinglunliang"></i>
+                                5591
+                            </span>
+                    </span>
+                    </div>
+                    <h3>
+                        <a href="#">测试标题测试标题测试标题</a>
+                    </h3>
+                </li>
+            </ul>
+        </main>
+        
     </div>
-`
+`, 'text/html').body.firstChild
+
+document.body.appendChild(template)
 /* ====================================================================================== */
--- src/mobile-adaptation/demo-bilibili/with-vw/index.js
+++ src/mobile-adaptation/demo-bilibili/with-vw/index.js
@@ -1,6 +1,55 @@
 import './index.scss'
-import { html } from '@/helper'
-import Logo from '../images/logo.webp'
+import { html, listEl } from '@/helper'
+import Video01 from '../images/01.webp'
+import Video02 from '../images/02.webp'
+import Video03 from '../images/02.webp'
+
+const videoList = [
+    {
+        img: Video01,
+        title: '一个人在看你，你会怎么说？',
+        play: '73.7万',
+        comment: '5591'
+    },
+    {
+        img: Video02,
+        title: '这是一个标题，这是一个标题',
+        play: '73.7万',
+        comment: '5591'
+    },
+    {
+        img: Video01,
+        title: '我在bilibili看视频，我在bilibili看视频',
+        play: '73.7万',
+        comment: '5591'
+    },
+    {
+        img: Video03,
+        title: '什么人都可以成为一个人，我是一个人',
+        play: '73.7万',
+        comment: '5591'
+    },
+    {
+        img: Video01,
+        title: '谁会不想在bilibili看视频',
+        play: '73.7万',
+        comment: '5591'
+    },
+    {
+        img: Video02,
+        title: '不管你是一个人，还是一个群体，你都可以成为一个人',
+        play: '73.7万',
+        comment: '5591'
+    },
+    {
+        img: Video02,
+        title: '每天都是有很多人在看视频，但是我不会',
+        play: '73.7万',
+        comment: '5591'
+    }
+]
+
+
 
 const template = new window.DOMParser().parseFromString(html`
     <div class="main">
@@ -18,6 +67,60 @@ const template = new window.DOMParser().parseFromString(html`
                 <a href="#">下载 App</a>
             </div>
         </header>
+        
+        <nav class="nav">
+            <ul class="nav-list">
+                <li >首页</li>
+                <li class="active">动画</li>
+                <li>番剧</li>
+                <li>国创</li>
+                <li>音乐</li>
+                <li>舞蹈</li>
+                <li>电影</li>
+                <li>番剧</li>
+                <li>国创</li>
+                <li>音乐</li>
+            </ul>
+            <div class="nav-arrow">
+                <i class="iconfont icon-xiangxia"></i>
+            </div>
+        </nav>
+        
+        <main class="main-content">
+            <ul class="videos">
+                ${
+                    listEl(videoList, (index, item) => {
+                        const { 
+                            title,
+                            img,
+                            play,
+                            comment
+                        } = item
+                        return html`
+                            <li class="video-item">
+                                <div class="video">
+                                    <img class="video-item-img" alt="视频" src="${img}" />
+                                    <span>
+                                        <span>
+                                            <i class="iconfont icon-shipinbofangliang"></i>
+                                            ${play}
+                                        </span>
+                                        <span>
+                                            <i class="iconfont icon-pinglunliang"></i>
+                                            ${comment}
+                                        </span>
+                                    </span>
+                                </div>
+                                <h3>
+                                    <a href="#">${title}</a>
+                                </h3>
+                            </li>
+                            `
+                    })
+                }
+            </ul>
+        </main>
+        
     </div>
 `, 'text/html').body.firstChild
 
