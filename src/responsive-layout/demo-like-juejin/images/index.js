e9669c3489feb72a07f0c7e631bf7781b84c46d6
zhangli <2475934003@qq.com>
2025-06-22 18:50:33 +0800

    feat:仿掘金响应式-header

---
+++ src/responsive-layout/demo-like-juejin/images/index.js
@@ -0,0 +1,4 @@
+export const JueJinLogo = 'https://lf-web-assets.juejin.cn/obj/juejin-web/xitu_juejin_web/e08da34488b114bd4c665ba2fa520a31.svg'
+export const JueJinLogoMobile = 'https://lf-web-assets.juejin.cn/obj/juejin-web/xitu_juejin_web/6c61ae65d1c41ae8221a670fa32d05aa.svg'
+export const VipEntry = 'https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/ffd3e238ee7f46eab42bf88af17f5528~tplv-k3u1fbpfcp-image.image#?w=25&h=26&s=5968&e=svg&a=1&b=dacbbc'
+
--- src/responsive-layout/demo-like-juejin/index.js
+++ src/responsive-layout/demo-like-juejin/index.js
@@ -1,9 +1,68 @@
 import './index.scss'
-import { html, parseHTML } from '@/helper'
+import './head.scss'
+
+import { html, parseHTML, listEl } from '@/helper'
+import { JueJinLogo, JueJinLogoMobile, VipEntry } from './images'
+import ProfilePhoto from './images/profile_photo.webp'
+
+const navList = [
+    'AI Coding',
+    '沸点',
+    '课程',
+    '直播',
+    '活动',
+    'AI刷题',
+    'APP',
+    '插件'
+]
 
 const template = parseHTML(html`
     <div class="page">
-        <h1>hello world</h1>
+        <header class="head">
+            <div class="logo">
+                <img class="logo-img" src="${JueJinLogo}" alt="logo">
+                <img class="logo-mobile" src="${JueJinLogoMobile}" alt="logo">
+                
+            </div>
+            
+            <div class="nav">
+                <div class="home-page nav-list-item active">首页</div>
+                <ul class="nav-list">
+                    ${ listEl(navList, (_, navItem) => {
+                        return html`<li class="nav-list-item">${navItem}</li>`
+                    }) }
+                </ul>
+            </div>
+            
+            <div class="head-search">
+                <div class="input-box">
+                    <input placeholder="搜索稀土掘金" />
+                </div>
+                <div class="head-search-icon">
+                    <i class="iconfont icon-sousuo"></i>
+                </div>
+            </div>
+            
+            <div class="creator-center">
+                <span>创作者中心</span>
+                <div class="icon">
+                    <i class="iconfont icon-sanjiao_xia"></i>
+                </div>
+            </div>
+            
+            <div class="vip">
+                <i class="iconfont icon-huiyuanguanli-copy"></i>
+                <span>会员</span>
+            </div>
+            
+            <div class="notify">
+                <i class="iconfont icon-xiaoxitongzhi"></i>
+            </div>
+            
+            <div class="profile-photo">
+                <img src="${ProfilePhoto}" alt="头像">
+            </div>
+        </header>
     </div>
 `)
 
